a918187e646d4f4fa3e863d0ba55b41a7b5c0894
root <adhikariratna52@gmail.com>
2017-01-29 12:08:54 +0545

    debugged staticfiles_dirs and finally serving static files

--- src/ecompro/settings.py
+++ src/ecompro/settings.py
@@ -124,6 +124,6 @@ if DEBUG:
     MEDIA_URL = '/media/'
     STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static','static-only')
     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static','media')
-    STATICFILES_DIR = (
+    STATICFILES_DIRS = (
         os.path.join(os.path.dirname(BASE_DIR),'static','static'),
     )
